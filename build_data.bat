ced7498b7b15096bda3e4b86ebf31747a8143617
PCMan <PCMan@ed530acc-6d03-0410-930a-b2293188b984>
2006-05-12 23:50:46 +0000

    修正建造 dat 檔的批次檔。

--- build_data.bat
+++ build_data.bat
@@ -1,17 +1,22 @@
 @echo 開始建造新酷音輸入法詞庫檔(Big5)
-dat2bin.exe big5
+cd big5
+dat2bin.exe
 del *.dat_src
 ren ch_index.dat *.dat_src
 ren ph_index.dat *.dat_src
 ren fonetree.dat *.dat_src
 ren *.dat_bin *.dat
+
+cd ..
 
 @echo 開始建造新酷音輸入法詞庫檔 (UTF-8)
-dat2bin.exe utf-8
+cd utf-8
+dat2bin.exe
 del *.dat_src
 ren ch_index.dat *.dat_src
 ren ph_index.dat *.dat_src
 ren fonetree.dat *.dat_src
 ren *.dat_bin *.dat
 
+@echo 建造完成！
 pause
