c12e8184e45ad0656a340abdcde754623ea8268f
PCMan <PCMan@ed530acc-6d03-0410-930a-b2293188b984>
2006-05-12 23:43:23 +0000

    將 big5 版本和 utf-8 版本的詞庫檔案、以及 dat2bin.exe 分到不同目錄下，並且修改 build_data.bat 使之能夠同時 build 兩種版本。

--- build_data.bat
+++ build_data.bat
@@ -1,8 +1,17 @@
-@echo 開始建造新酷音輸入法詞庫檔...
-dat2bin.exe
+@echo 開始建造新酷音輸入法詞庫檔(Big5)
+dat2bin.exe big5
 del *.dat_src
 ren ch_index.dat *.dat_src
 ren ph_index.dat *.dat_src
 ren fonetree.dat *.dat_src
 ren *.dat_bin *.dat
+
+@echo 開始建造新酷音輸入法詞庫檔 (UTF-8)
+dat2bin.exe utf-8
+del *.dat_src
+ren ch_index.dat *.dat_src
+ren ph_index.dat *.dat_src
+ren fonetree.dat *.dat_src
+ren *.dat_bin *.dat
+
 pause
